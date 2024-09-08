217f72eacbfb8ef9908c505c596501c0fa0a0cee
suhaakach <suhakach20@hotmail.com>
2024-09-08 03:13:27 -0400

    decrypted 2nd message

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -5,3 +5,18 @@ encrypted_message = encrypted_file.readline()
 encrypted_file.close()
 
 # Write Code Here
+#turns hidden message to a list
+key = list(encrypted_message)
+
+# performs decryption by picking the second letter, going halfway through the line, and moving 2 letters at a time.
+for index in range(1, len(key) // 2, 2):
+    # brings the character index from the opposite direction
+    ending_index = len(key) - index - 1
+    
+    # swaps characters
+    key[index], key[ending_index] = key[ending_index], key[index]
+
+# joins the list and creates a string
+decrypted_message = ''.join(key)
+
+print(decrypted_message)
